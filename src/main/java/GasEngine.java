7eceddeef2bf1e1eef60482c44a4ea4f8f14cd1b
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-24 11:08:54 +0300

    feat: Add GasEngine implementation

---
+++ src/main/java/GasEngine.java
@@ -0,0 +1,19 @@
+public class GasEngine extends BaseEngine{
+
+    @Override
+    public void startEngine(){
+        System.out.println("Started Gas Engine, engine speed= "+speed);
+    }
+
+    @Override
+    public void stopEngine(){
+        if (speed == 0) {
+            System.out.println("Stopped Gas Engine");
+        }else{
+            System.out.println("can't stop the car while engine speed>0," +
+                    "please decelerate first");
+        }
+    }
+
+
+}
